24b52d4644967cded64cedea7fd705b665a08c4a
Kartikey bharti <56760656+Kartikeybharti8@users.noreply.github.com>
2022-05-01 09:49:35 +0530

    w7/14

---
+++ week7/merge2_sorted_arr.py
@@ -0,0 +1,17 @@
+def mergeArrays(a,b):
+  c= []
+  i=0;j=0
+  while(i<len(a) and j<len(b)):
+    if(a[i]<b[j]):
+      c.append(a[i])
+      i+=1
+    else:
+      c.append(b[j])
+      j+=1
+  while(i<len(a)):
+    c.append(a[i])
+    i+=1;
+  while(j<len(b)):
+    c.append(b[j])
+    j+=1
+  return c
